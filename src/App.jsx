4e0514380f70098f93c724140194f25b325dec20
suhailWebDev <mdsuhail1410@gmail.com>
2025-01-23 16:05:45 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
